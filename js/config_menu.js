70999ed64041250379ab209a75fe091344307657
mbentz-uf <mbentz@ufl.edu>
2022-07-21 14:50:45 -0400

    Minor cleanup

--- js/config_menu.js
+++ js/config_menu.js
@@ -1,89 +1,86 @@
-const json_lint_button = $("<input type='button' onclick='prettyPrint(this)' value='Validate JSON'></input>");
-const sample_map = JSON.stringify(
-        {
-            "source_field_1" : "target_field_1",
-            "source_field_2" : "target_field_2"
-        },
-        undefined,
-        2
-        );
-$(document).ready(function() {
-    print('ready');
-    const source_codebook = `<a target='_blank' href='${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${CBCPD.sourceProjectId}'><button>Source codebook</button></a>`;
-    const target_codebook = `<a target='_blank' href='${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${CBCPD.thisProjectId}'><button>Target codebook</button></a>`;
-    var $modal = $('#external-modules-configure-modal');
-    var lastHoveredProjectID;
+const jsonLintButton = $('<input type=\'button\' onclick=\'prettyPrint(this)\' value=\'Validate JSON\'></input>');
+const sampleMap = `[
+        "source_field_1",
+        "source_field_2",
+        "source_field_3"
+]`;
+$(document).ready(() => {
+  print('ready');
+  const sourceCodebook = `<a target='_blank' href='${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${CBCPD.sourceProjectId}'><button>Source codebook</button></a>`;
+  const targetCodebook = `<a target='_blank' href='${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${CBCPD.thisProjectId}'><button>Target codebook</button></a>`;
+  const $modal = $('#external-modules-configure-modal');
+  let lastHoveredProjectID;
 
-    /* Dynamically updating the href of the source codebook is accomplished by 
+  /* Dynamically updating the href of the source codebook is accomplished by
      * capturing the last hovered project in the select dropdown and updating
      * the href when a new project is selected.
-    
+
      * NOTE: Dynamically updating the href cannot be accomplished without the mouseenter event listener.
      * This is because the change event on the select list completes before updating, resulting
      * in the previous option being available rather than the new option that was selected. By keeping
      * track of the last hovered Project in the dropdown, the href can be properly updated.
-    **/
-    var setSourceCodebook = function() {
-        // Attach mouseenter event listener to project list options to capture last hovered project
-        $('body').on('mouseenter', 'li.select2-results__option', function(event) {
-            var projectText = $(this).text();
-            lastHoveredProjectID = $('tr[field="target_pid"]').find('option:contains("'+projectText+'")').attr('value');
-        });
+    * */
+  const setSourceCodebook = function () {
+    // Attach mouseenter event listener to project list options to capture last hovered project
+    $('body').on('mouseenter', 'li.select2-results__option', function (event) {
+      const projectText = $(this).text();
+      lastHoveredProjectID = $('tr[field="target_pid"]').find(`option:contains("${projectText}")`).attr('value');
+    });
 
-        // Attach change event listener to the project select list to update href
-        $('tr[field="target_pid"]').find('select').on('change', function(event) {
-            var updatedHref = `${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${lastHoveredProjectID}`;
-            $("tr[field='codebook_shortcuts'] a").first().attr('href', updatedHref);
-        });        
+    // Attach change event listener to the project select list to update href
+    $('tr[field="target_pid"]').find('select').on('change', (event) => {
+      const updatedHref = `${app_path_webroot_full}${app_path_webroot.slice(1)}Design/data_dictionary_codebook.php?pid=${lastHoveredProjectID}`;
+      $('tr[field=\'codebook_shortcuts\'] a').first().attr('href', updatedHref);
+    });
+  };
+
+  $modal.on('show.bs.modal', function () {
+    if ($(this).data('module') != CBCPD.modulePrefix) {
+      return;
     }
 
-    $modal.on('show.bs.modal', function() {
-        if ( $(this).data('module') != CBCPD.modulePrefix ) {
-            return;
-        }
+    if (typeof ExternalModules.Settings.prototype.resetConfigInstancesOld === 'undefined') {
+      ExternalModules.Settings.prototype.resetConfigInstancesOld = ExternalModules.Settings.prototype.resetConfigInstances;
+    }
 
-        if (typeof ExternalModules.Settings.prototype.resetConfigInstancesOld === 'undefined') {
-            ExternalModules.Settings.prototype.resetConfigInstancesOld = ExternalModules.Settings.prototype.resetConfigInstances;
-        }
+    ExternalModules.Settings.prototype.resetConfigInstances = function () {
+      ExternalModules.Settings.prototype.resetConfigInstancesOld();
+      if ($modal.data('module') != CBCPD.modulePrefix) {
+        return;
+      }
+      // Force the descriptive field to show codebook buttons
+      $('tr[field=\'codebook_shortcuts\']')
+        .children(':first')
+        .removeAttr('colspan')
+        .next() // hijack existing empty td for buttons
+        .html(sourceCodebook + targetCodebook)
+        .attr('colspan', 2);
 
-        ExternalModules.Settings.prototype.resetConfigInstances = function() {
-            ExternalModules.Settings.prototype.resetConfigInstancesOld();
-            if ( $modal.data('module') != CBCPD.modulePrefix ) {
-                return;
-            }
-            // Force the descriptive field to show codebook buttons
-            $("tr[field='codebook_shortcuts']")
-                .children(":first")
-                .removeAttr('colspan')
-                .next() // hijack existing empty td for buttons
-                .html(source_codebook + target_codebook)
-                .attr('colspan', 2);
+      const $mappingFields = $('textarea[name*=\'mapping___\']');
+      $mappingFields
+      // .attr('placeholder', sample_map)
+        .attr('placeholder', sampleMap)
+        .siblings().remove(); // prevent duplicate lint buttons for a mapping field
+      $mappingFields.after(jsonLintButton.clone());
+      // TODO: force validation of all JSON fields _before_ save
 
-            let $mappingFields = $("textarea[name*='mapping___']");
-            $mappingFields
-                .attr('placeholder', sample_map)
-                .siblings().remove(); // prevent duplicate lint buttons for a mapping field
-            $mappingFields.after(json_lint_button.clone());
-            // TODO: force validation of all JSON fields _before_ save
-
-            // Dynamically update source codebook
-            setSourceCodebook();
-        }
-
-    });
+      // Dynamically update source codebook
+      setSourceCodebook();
+    };
+  });
 });
 
-function prettyPrint(element) {
-    let $field = $(element).prev();
-    let ugly = $field.val();
-    if (ugly) {
-        try {
-            let pretty = JSON.stringify(JSON.parse(ugly), undefined, 2);
-            $field.val(pretty);
-        } catch (err) {
-            if (err instanceof SyntaxError) {
-                alert("There is an error in your JSON syntax:\n" + err.message);
-            }
-        }
-    }
-}
+// function prettyPrint (element) {
+//   const $field = $(element).prev()
+//   const ugly = $field.val()
+//   if (ugly) {
+//     try {
+//       const pretty = JSON.stringify(JSON.parse(ugly), undefined, 2)
+//       $field.val(pretty)
+//     } catch (err) {
+//       if (err instanceof SyntaxError) {
+//         alert('There is an error in your JSON syntax:\n' + err.message)
+//       }
+//     }
+//   }
+// }
--- js/custom_data_search.js
+++ js/custom_data_search.js
@@ -1,157 +1,162 @@
-$(document).ready(function () {
-    // TODO(mbentz-uf): learn what these lines do
-    if (CBCPD.limit_fields) {
-        // field selector options are initially for the target, not source project
-        // replace them with only those defined in the project config
-        $('#field_select').empty();
-        $.each(CBCPD.source_fields_mapping, function (key, label) {
-            $('#field_select').append($('<option></option>').val(key).html(label));
-        });
-    } else {
-        $('#field_select').parent().parent().hide();
-    }
+/**
+ *
+ * @param {string} copyData
+ */
+function showDataConfirmModal(json) {
+  // const caregiverDemographics = JSON.parse(json);
+  // show the modal
+  $('#dialog-data-stp').dialog('open');
 
-    // setting up the dialog for the search confirmation before copying
-    $('#dialog-data-stp').dialog({
-        autoOpen: false,
-        draggable: true,
-        resizable: true,
-        closeOnEscape: true,
-        minWidth: 500,
-        modal: true,
-        open: function () {
-            // clicking the background of the modal closes the modal
-            $('.ui-widget-overlay').bind('click', function () {
-                $('#dialog-data-stp').dialog('close');
-            })
-        }
-    });
+  // hold onto the data in its current form
+  // $('#dialog-data-stp').data('copyData', copyData);
 
-    // TODO(mbentz-uf): make this configurable
-    // Insert a button to check demographics
-    $('#form-title').after(
-    // $('#form_menu_description_label').after(
-        $('<button />')
-            .html('Check')
-            .attr(
-                {
-                    type: 'button',
-                    id: 'Check',
-                    onlcick: 'showPatientInfo()',
-                    class: 'btn btn-info btn-sm'
-                }
-            )
-    );
+  // clear the rows from any previous search
+  $('#body-for-stp-modal').empty();
 
-    // Add on-click listener to demographic check button
-    $('#Check').on('click', () => {
-        // `recordId` is the equivalent of ADC Subject ID for the ADRC project
-        const recordId = $('#record_id-tr > td:last-child').text();
-        getCaregiverInfo(recordId);
-    });
-});
+  for (const [key, value] of Object.entries(json)) {
+    // Add the rows from the current search
+    $('#body-for-stp-modal').append(`<tr><td>${key}</td><td>${value}</td></tr>`);
+  }
+}
 
 // `GET` Caregiver demographic data from `ajaxpage.php`
-const getCaregiverInfo = function(recordId) {
-    const urlParams = new URLSearchParams(window.location.search);
-    $.get({
-        url: CBCPD.ajaxpage,
-        data: {
-            recordId: recordId,
-            instrument: urlParams.get('page')
+const getCaregiverInfo = (recordId) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  $.get({
+    url: CBCPD.ajaxpage,
+    data: {
+      recordId,
+      instrument: urlParams.get('page'),
+    },
+  })
+    .done((data) => {
+      const responseData = JSON.parse(data);
+      showDataConfirmModal(responseData);
+    });
+};
+
+$(document).ready(() => {
+  // TODO(mbentz-uf): learn what these lines do
+  if (CBCPD.limit_fields) {
+    // field selector options are initially for the target, not source project
+    // replace them with only those defined in the project config
+    $('#field_select').empty();
+    $.each(CBCPD.source_fields_mapping, (key, label) => {
+      $('#field_select').append($('<option></option>').val(key).html(label));
+    });
+  } else {
+    $('#field_select').parent().parent().hide();
+  }
+
+  // setting up the dialog for the search confirmation before copying
+  $('#dialog-data-stp').dialog({
+    autoOpen: false,
+    draggable: true,
+    resizable: true,
+    closeOnEscape: true,
+    minWidth: 500,
+    modal: true,
+    open() {
+      // clicking the background of the modal closes the modal
+      $('.ui-widget-overlay').bind('click', () => {
+        $('#dialog-data-stp').dialog('close');
+      });
+    },
+  });
+
+  // TODO(mbentz-uf): make this configurable
+  // Insert a button to check demographics
+  $('#form-title').after(
+    // $('#form_menu_description_label').after(
+    $('<button />')
+      .html('Check')
+      .attr(
+        {
+          type: 'button',
+          id: 'Check',
+          onlcick: 'showPatientInfo()',
+          class: 'btn btn-info btn-sm',
         },
-    })
-        .done(function (data) {
-            responseData = JSON.parse(data);
-            showDataConfirmModal(responseData);
-        });
-}
+      ),
+  );
 
-function pasteValues(values) {
-    console.log(values);
-    // for (let [key, value] of Object.entries(values)) {
-    //     let $target_field = $(`input[name='${key}']`);
-    //     if ($target_field.length == 0) {
-    //         // not found by name attr, field may be present as a dropdown
-    //         selectFromDropdown(key, value);
-    //     }
-    //     // radio and regular text boxes
-    //     if ($target_field.attr('class') == 'hiddenradio') {
-    //         // collect all radio fields in all layouts
-    //         let $inputs = $target_field.siblings('[class*="choice"]');
-    //         // select radio assuming target coded value matches source coded value
-    //         $inputs.find(`[value='${value}']`).click();
-    //     } else {
-    //         // FIXME: does not honor desired date formatting
-    //         $target_field.val(`${value}`);
-    //         $target_field.blur();
-    //     }
-    // }
-}
+  // Add on-click listener to demographic check button
+  $('#Check').on('click', () => {
+    // `recordId` is the equivalent of ADC Subject ID for the ADRC project
+    const recordId = $('#ptid-tr input').val();
+    getCaregiverInfo(recordId);
+  });
+});
 
-function selectFromDropdown(key, value) {
-    const $target_row = $(`tr[sq_id='${key}']`);
-    const $ac_target_field = $($target_row.find('input')[0]); // ac = auto complete
-    const $select_field = $(`select[name='${key}']`);
+// function pasteValues (values) {
+//   console.log(values)
+//   // for (let [key, value] of Object.entries(values)) {
+//   //     let $target_field = $(`input[name='${key}']`);
+//   //     if ($target_field.length == 0) {
+//   //         // not found by name attr, field may be present as a dropdown
+//   //         selectFromDropdown(key, value);
+//   //     }
+//   //     // radio and regular text boxes
+//   //     if ($target_field.attr('class') == 'hiddenradio') {
+//   //         // collect all radio fields in all layouts
+//   //         let $inputs = $target_field.siblings('[class*="choice"]');
+//   //         // select radio assuming target coded value matches source coded value
+//   //         $inputs.find(`[value='${value}']`).click();
+//   //     } else {
+//   //         // FIXME: does not honor desired date formatting
+//   //         $target_field.val(`${value}`);
+//   //         $target_field.blur();
+//   //     }
+//   // }
+// }
 
-    // used to handle cases where the value provided is the displayed value of the desired option,
-    // rather than the coded value (value attribute)
-    const displayed_option_value = $select_field
-        .children()
-        .filter((i, e) => {
-            return ($(e).html() == value);
-        })
-        .val();
+// function selectFromDropdown (key, value) {
+//   const $targetRow = $(`tr[sq_id='${key}']`)
+//   const $ac_target_field = $($targetRow.find('input')[0]) // ac = auto complete
+//   const $select_field = $(`select[name='${key}']`)
 
-    // autocomplete fields
-    if ($ac_target_field.attr('class') == 'x-form-text x-form-field rc-autocomplete ui-autocomplete-input') {
-        // the non-coded value must be put in the text box to allow the user to see the pipe occured
-        // if displayed_value is undefined, this function sets the value to nothing
-        const displayed_value = $select_field
-            .children(`[value='${value}']`)
-            .html();
-        $ac_target_field.val(displayed_value);
+//   // used to handle cases where the value provided is the displayed value of the desired option,
+//   // rather than the coded value (value attribute)
+//   const displayed_option_value = $select_field
+//     .children()
+//     .filter((i, e) => {
+//       return ($(e).html() == value)
+//     })
+//     .val()
 
-        if ($ac_target_field.val() != value && displayed_option_value != undefined) {
-            // TODO: handle the possibilty that this value could go in an "other" field behind branching logic
-            $ac_target_field.val(value);
-        }
-        return;
-    }
+//   // autocomplete fields
+//   if ($ac_target_field.attr('class') == 'x-form-text x-form-field rc-autocomplete ui-autocomplete-input') {
+//     // the non-coded value must be put in the text box to allow the user to see the pipe occured
+//     // if displayed_value is undefined, this function sets the value to nothing
+//     const displayed_value = $select_field
+//       .children(`[value='${value}']`)
+//       .html()
+//     $ac_target_field.val(displayed_value)
 
-    // non autocomplete fields
-    $select_field.val(value);
-    if ($select_field.val() != value && displayed_option_value != undefined) {
-        // TODO: handle the possibilty that this value could go in an "other" field behind branching logic
-        $select_field.val(displayed_option_value);
-    }
-}
+//     if ($ac_target_field.val() != value && displayed_option_value != undefined) {
+//       // TODO: handle the possibilty that this value could go in an "other" field behind branching logic
+//       $ac_target_field.val(value)
+//     }
+//     return
+//   }
 
-function showDataConfirmModal(copyData) {
-    // show the modal
-    $('#dialog-data-stp').dialog('open');
+//   // non autocomplete fields
+//   $select_field.val(value)
+//   if ($select_field.val() != value && displayed_option_value != undefined) {
+//     // TODO: handle the possibilty that this value could go in an "other" field behind branching logic
+//     $select_field.val(displayed_option_value)
+//   }
+// }
 
-    // hold onto the data in its current form
-    $('#dialog-data-stp').data('copyData', copyData);
-
-    // clear the rows from any previous search
-    $('#body-for-stp-modal').empty();
-    for (let [key, value] of Object.entries(copyData)) {
-        // Add the rows from the current search
-        $('#body-for-stp-modal').append('<tr><td>' + key + '</td><td>' + value + '</td></tr>');
-    }
-
-}
-
-function hideDataConfirmModal(isCopy) {
-    // close the modal
-    $('#dialog-data-stp').dialog('close');
-    // get the data from the html
-    copydata = $('#dialog-data-stp').data('copyData');
-    if (isCopy > 0) {
-        // copy the data into the form
-        pasteValues(copydata);
-    }
-    // clean up afterwards
-    $('#dialog-data-stp').removeData('copyData');
-}
+// function hideDataConfirmModal (isCopy) {
+//   // close the modal
+//   $('#dialog-data-stp').dialog('close')
+//   // get the data from the html
+//   const copydata = $('#dialog-data-stp').data('copyData')
+//   if (isCopy > 0) {
+//     // copy the data into the form
+//     pasteValues(copydata)
+//   }
+//   // clean up afterwards
+//   $('#dialog-data-stp').removeData('copyData')
+// }
